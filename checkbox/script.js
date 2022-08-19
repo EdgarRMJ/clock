8767601b1e921ec7638bfe4143675cfd575304f1
EdgarRMJ <antonio.romero.mejias@gmail.com>
2022-08-19 12:41:20 -0400

    first commit

---
+++ checkbox/script.js
@@ -0,0 +1,23 @@
+window.onload = function()
+{
+    
+    var myForm = document.getElementById("myForm");
+    var submitButton = document.getElementById("myForm").submitButton;
+    
+    var info = document.getElementById("info");
+
+    
+    submitButton.onclick = function(e)
+    {
+        var tmpString = "";
+        for (var i = 0; i < myForm.courseName.length; i++)
+        {
+            if(myForm.courseName[i].checked)
+            tmpString += myForm.courseName[i].value + " ";
+        }
+        
+        info.innerHTML += tmpString + "<br>";
+        //e.preventDefault();
+    }
+}
+
